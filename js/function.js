2631fd749eba62fe061571761678759511140869
1987_ Mohammad Habib Ullah <mohammadhabibullah937@gmail.com>
2024-09-23 02:57:20 +0600

    add

--- js/function.js
+++ js/function.js
@@ -1,20 +1,12 @@
 
-
 function getInputFieldValueById(value){
-
     let result=document.getElementById(value).value
-
     let result_int=parseFloat(result)
-
     return result_int
 }
-
 function getTextFieldValueById(value){
-
     let result=document.getElementById(value).innerText
-
     let result_int=parseFloat(result)
-
     return result_int
 }
 
